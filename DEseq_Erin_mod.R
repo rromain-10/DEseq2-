9273f4603ae3914066fd0468bb8ee61e50744e02
Romario Romain <romarioromain@Romarios-MacBook-Air.local>
2020-01-14 14:15:48 -0700

    1/14/2020

--- DEseq_Erin_mod.R
+++ DEseq_Erin_mod.R
@@ -32,21 +32,21 @@ biocLite("DESeq2")
 ## When prompted to install (a/s) all or some of the dependent packages, move your cursor down to the console and type "a" for all
 
 # Install DESeq2:
-if (!requireNamespace("BiocManager", quietly = TRUE))
-        install.packages("BiocManager")
-BiocManager::install("DESeq2")
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#        install.packages("BiocManager")
+#BiocManager::install("DESeq2")
 
 # Install 'apeglm'
-BiocManager::install("apeglm")
+#BiocManager::install("apeglm")
 
 # install corrplot:
-install.packages("corrplot")
+#install.packages("corrplot")
 
 #Install pretty heatmap - pheatmap: https://cran.r-project.org/web/packages/pheatmap/index.html
-install.packages("pheatmap")
+#install.packages("pheatmap")
 
 #Install RColorBrewer: https://cran.r-project.org/web/packages/RColorBrewer/index.html
-install.packages("RColorBrewer")
+#install.packages("RColorBrewer")
 # 
 # ######### DONE WITH: FIRST TIME USE ONLY SECTION ##############
 # ################################
@@ -72,18 +72,18 @@ library(pheatmap)
 getwd()
 
 #Set this to your working directory:
-setwd("/Users/romarioromain/OneDrive - Colostate/RR_ARPE_DELUCA_COLLAB/DEseq/02_scripts/DEseq2")
+setwd("/Users/romarioromain/OneDrive - Colostate/RR_ARPE_DELUCA_COLLAB/DEseq/DEseq2")
 getwd()
-countsData <- read.table(file = "../../01_input/counts.txt", header = FALSE, row.names = 1, skip = 2) 
+countsData <- read.table(file = "/Users/romarioromain/OneDrive - Colostate/RR_ARPE_DELUCA_COLLAB/DEseq/DEseq2/all_counts2.text", header = TRUE, row.names = 1, skip = 2) 
 head(countsData)
 dim(countsData)
 class(countsData)
 
 # Read in the metadata
-metadata1 <- read.table(file = "../../01_input/RR_ARPE_Deluca_Collab_manifest.txt", header = FALSE) # import the data
-metadata1
-colnames(metadata1) <- c("fasta1", "fasta2", "names1", "rep","sample")
-metadata1
+metadata <- read.table(file = "/Users/romarioromain/OneDrive - Colostate/RR_ARPE_DELUCA_COLLAB/DEseq/DEseq2/RR_ARPE_Deluca_Collab_manifest.txt", header = FALSE) # import the data
+metadata
+colnames(metadata) <- c("fasta1", "fasta2", "names", "rep","sample")
+metadata
 
 # Organize the countsData file.
 # Notice that the countsData file doesn't have any column headers:
@@ -91,11 +91,11 @@ head(countsData)
 
 # Let's give countsData some columns names. The first names will be... chr', 'start', etc...
 # The last names will be names for each sample. We can pull those names from metadata1:
-as.vector(metadata1$names1)
-as.vector(metadata1$sample)
+as.vector(metadata$names)
+as.vector(metadata$sample)
 
 # Name countsData columns headers:
-colnames(countsData) <- c("chr", "start", "stop", "strand", "length", as.vector(metadata1$names1))
+colnames(countsData) <- c("chr", "start", "stop", "strand", "length", as.vector(metadata$names))
 colnames(countsData)
 head(countsData)
 
@@ -107,28 +107,27 @@ head(countsData)
 # Subset the countsData 
 head(countsData)
 dim(countsData)
-head(countsData[,6:16])
+head(countsData[,6:22])
+countsData_count_only <- (countsData[,6:22])
 
 # Save just the subset as an object called cts:
-cts <- as.matrix(countsData[,6:16])
+cts <- as.matrix(countsData_count_only, sep="\t")
 head(cts)
 
 # Next we need to make an information called coltable. We can make this out of the metadata table.
-class(metadata1)
+class(metadata)
 # Reorganize the metadata table so the names1 column are now row headers
-metadata1
-rownames(metadata1)<- metadata1$names1
-metadata1
+metadata
+rownames(metadata)<- metadata$names
+metadata
 
-coldata <- metadata1[,c("sample", "rep","names1", "fasta1")]
+coldata <- metadata[,c("sample", "rep", "names")]
 coldata
 coldata$sample <- as.factor(coldata$sample)
-coldata$names1 <- as.factor(coldata$names1)
-coldata$rep <- as.factor(coldata$rep)
-coldata$fasta1 <- as.factor(coldata$fasta1)
-rownames(coldata) <- metadata1$names1
+coldata$names <- as.factor(coldata$names)
+rownames(coldata) <- metadata$names
 coldata$sample
-coldata$names1 
+coldata$names
 colnames(cts)
 
 # One thing we need to explicitly check. The rownames of coldata need to exactly match the colnames of cts.
@@ -137,7 +136,8 @@ all(rownames(coldata) == colnames(cts))
 
 dds <- DESeqDataSetFromMatrix(countData = cts,
                               colData = coldata,
-                              design = ~ sample + rep)
+                              design = ~ sample,
+                              ignoreRank = TRUE)
 
 
 ################# PRE-FILTERING -- FILTER FOR PRESENT GENES: ################# 
@@ -147,7 +147,7 @@ dds <- DESeqDataSetFromMatrix(countData = cts,
 # keep <- rowSums(counts(dds1)) >= 1
 # dds1 <- dds1[keep,]
 
-keep <- rowSums(counts(dds)) >= 1
+keep <- rowSums(counts(dds)) >= 500
 dds <- dds[keep,]
 
 # How many did we exclude?
@@ -157,8 +157,8 @@ coldata
 
 ################### NOTE ON FACTOR LEVELS ###################
 # Organize the categories based on what makes sense:
-dds2$sample <- factor(dds$sample)
-dds2$sample
+dds$sample <- factor(dds$sample)
+dds$sample
 
 # PERFORM DESEQ2 analysis:
 ####### This is where the magic happens! ########### 
@@ -175,9 +175,24 @@ head(dds)
 # dds1$sizeFactor
 # head(counts(dds, normalized = TRUE))
 # head(counts(dds, normalized = FALSE))
-dds2$sizeFactor
+dds$sizeFactor
 head(counts(dds, normalized = TRUE))
 head(counts(dds, normalized = FALSE))
+
+# Calculate the distances between each sample
+sampleDists <- dist(t(assay(rld))) # calculate distance matrices:
+sampleDistMatrix <- as.matrix(sampleDists) #convert from data.frame -> matrix
+rownames(sampleDistMatrix) <- colnames(rld) # Add some labels
+colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255) #Pick some pretty colors
+
+# Draw the heatmap
+par(mfrow=c(1,1))
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors, width = 4, height = 4,
+         cluster_rows = FALSE) # Plot the heatmap
 
 ############## PERFORM LOG FOLD CHANGE SHRINKAGE FOR VISUALIZATION #####################
 
@@ -313,12 +328,12 @@ rownames(resLFC_T53D4_vs_Aktmyr)[idx]
 #negclt(water) vs Aktmyr
 res_Water_vs_Aktmyr <- results(dds,
                                lfc = 0.5,
-                               contrast=c("sample","Water","Aktmyr"))
+                               contrast=c("sample","negclt","Aktmyr"))
 
 summary(res_Water_vs_Aktmyr)
 
 resLFC_Water_vs_Aktmyr <- lfcShrink(dds,
-                                    coef="sample_Water_vs_Aktmyr", type='apeglm')
+                                    coef="sample_negclt_vs_Aktmyr", type='apeglm')
 
 summary(resLFC_Water_vs_Aktmyr)
 
@@ -356,11 +371,12 @@ par(mfrow=c(1,1))
 pheatmap(sampleDistMatrix,
          clustering_distance_rows=sampleDists,
          clustering_distance_cols=sampleDists,
-         col=colors, width = 4, height = 4) # Plot the heatmap
+         col=colors, width = 4, height = 4,
+         cluster_rows= TRUE) # Plot the heatmap
 
 ##### VOLCANO PLOTS:###################
 # Volcano plots are nice ways of displaying the fold change against the p-value.
-resultsNames(dds2)
+resultsNames(dds)
 
 #ARPE19 vs Aktmyr
 res_volcano_plot_ARPE19vsAktmyr <- lfcShrink(dds,coef="sample_ARPE19_vs_Aktmyr", type='apeglm') # Calculate padj without a lower lfc limit
@@ -534,24 +550,6 @@ dim(Down_in_RasV12vsAktmyr)
 # write(rownames(Down_in_Aktmyr), file = "../03_output/Genes Down Aktmyr.txt", sep = "\n")
 # write(rownames(resLFC_ARPE19_vs_Aktmyr), file = "../03_output/all_present_genes.txt", sep = "\n") # sometimes you need a control/background set of genes, too.
 
-# Select the significant subset of genes MekDD vs Aktmyr
-Up_in_MekDDvsAktmyr <- subset(resLFC_MekDD_vs_Aktmyr, padj < 0.05 & log2FoldChange > 0.5)
-Up_in_MekDDvsAktmyr  <- Up_in_MekDDvsAktmyr [order(Up_in_MekkDDvsAktmyr$padj),] #order them
-head(Up_in_MekDDvsAktmyr) # Check them
-dim(Up_in_MekDDvsAktmyr)
-# Select the significant subset of genes that are down-regulated in acetic acid
-Down_in_MekDDvsAktmyr<- subset(resLFC_MekDD_vs_Aktmyr, padj < 0.05 & log2FoldChange < -0.5)
-Down_in_MekDDvsAktmyr <- Down_in_MekDDvsAktmyr[order(Down_in_MekDDvsAktmyr$padj),]
-head(Down_in_MekDDvsAktmyr)
-dim(Down_in_MekDDvsAktmyr)
-
-# Save these lists to output files:
-# write(rownames(Up_in_Aktmyr), file = "../03_output/Genes Up in Aktmyr.txt", sep = "\n")
-# write(rownames(Down_in_Aktmyr), file = "../03_output/Genes Down Aktmyr.txt", sep = "\n")
-# write(rownames(resLFC_ARPE19_vs_Aktmyr), file = "../03_output/all_present_genes.txt", sep = "\n") # sometimes you need a control/background set of genes, too.
-############## MAKE HIERARCHICALLY CLUSTERED HEATMAPS OF ALL CHANGING GENES #####################
-
-
 # Select the significant subset of genes T53D4 vs Aktmyr
 Up_in_T53D4vsAktmyr <- subset(resLFC_T53D4_vs_Aktmyr, padj < 0.05 & log2FoldChange > 0.5)
 Up_in_T53D4vsAktmyr  <- Up_in_T53D4vsAktmyr [order(Up_in_T53D4vsAktmyr$padj),] #order them
@@ -591,21 +589,33 @@ dim(Down_in_WatervsAktmyr)
 # Let's loosen our restrictions on significance to all genes with any log fold change and adjusted p-values less than 0.1 (both are default)
 
 # Get ARPE19 vs RasV12 differentially expressed genes:
-res_ARPE19_vs_RasV12 <- results(dds2,
+res_ARPE19_vs_Akymyr <- results(dds,
                                 lfc = 0.5,
                                 contrast=c("sample","ARPE19","Aktmyr"))
 
 #Subset each results table for just the differentially expressed genes:
-ARPE19vsAktmyr<- subset(res_ARPE19_vs_RasV12 , padj < 0.05)
-dim(subset(res_ARPE19_vs_RasV12 , padj < 0.05))
+ARPE19vsAktmyr<- subset(res_ARPE19_vs_Akymyr , padj < 0.05)
+dim(subset(res_ARPE19_vs_Aktmyr , padj < 0.05))
+
+MekDDvsAktmyr<- subset(res_MekDD_vs_Aktmyr , padj < 0.05)
+dim(subset(res_MekDD_vs_Aktmyr , padj < 0.05))
+
+RasV12vsAktmyr <- subset(res_RasV12_vs_Aktmyr , padj < 0.05)
+dim(subset(res_RasV12_vs_Aktmyr , padj < 0.05))
+
+T53D4vsAktmyr<- subset(res_T53D4_vs_Aktmyr , padj < 0.05)
+dim(subset(res_T53D4_vs_Aktmyr , padj < 0.05))
+
+WatervsAktmyr<- subset(res_Water_vs_Aktmyr , padj < 0.05)
+dim(subset(res_Water_vs_Aktmyr , padj < 0.05))
 
 #Determine how many genes were captured and merge them:
-changing_genes <- rbind(ARPE19vsAktmyr)
+changing_genes<- rbind(ARPE19vsAktmyr, MekDDvsAktmyr, RasV12vsAktmyr, T53D4vsAktmyr, WatervsAktmyr  )
 dim(changing_genes)
 length(unique(rownames(changing_genes)))
 
 # Get all r-stabilized log values of all the data:
-rdl_all <- assay(rlog(dds2, blind=FALSE))
+rdl_all <- assay(rlog(dds, blind=FALSE))
 
 #Subset just the changing genes:
 changing_lrt_rdl <- subset(rdl_all, rownames(rdl_all) %in% rownames(changing_genes))
