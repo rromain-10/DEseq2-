7df946a37c05416f37078fd4c382113459c72f83
Romario Romain <romarioromain@Romarios-MacBook-Air.local>
2020-01-17 14:39:47 -0700

    added crucial rownames() and delete unused var

--- 20200114_DEseq_Erin_mod.R
+++ 20200114_DEseq_Erin_mod.R
@@ -138,13 +138,14 @@ library(ensembldb)
 library(biomaRt)
 ensembl = useDataset("hsapiens_gene_ensembl",mart=useMart("ENSEMBL_MART_ENSEMBL"))
 ctsnames = rownames(cts)
-ctsname_factor = factor(ctsnames, levels=ctsnames)
+
 # figure out filter,values,and attributes at http://www.ensembl.org/biomart/
 gene_names = getBM(mart = ensembl, filter='ensembl_gene_id', value=ctsnames, attributes=c('external_gene_name', 'ensembl_gene_id'))
 dim(gene_names) # 20002     2
 length(ctsnames) # 20003
 ctsnames = ctsnames[ ctsnames %in%  gene_names$ensembl_gene_id] # only one gene not found- ENSG00000254462, and it's all 0s
 length(ctsnames) # 20002
+rownames(gene_names) <- gene_names$ensembl_gene_id
 gene_names = gene_names[ctsnames,]
 cts=cts[rownames(cts) != 'ENSG00000254462',]# take out the 0 count gene
 rownames(cts) <- gene_names$external_gene_name
